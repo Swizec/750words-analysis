f8bdd018269c8eb89ece6c21842874b75bfb6e71
Swizec Teller <swizec@swizec.com>
2012-05-18 18:27:02 +0200

    length/time graphs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-(function basic_time(container) {
-
+function draw_time(container, data, title) {
     var
     d1    = [], d2 = [], d3 = [], d4 = [],
     start = new Date(DATA[0].date).getTime(),
@@ -7,11 +6,55 @@
     graph,
     i, x, o;
 
-    var collect = function (key) {
+
+    options = {
+        xaxis : {
+            mode : 'time',
+            labelsAngle : 45
+        },
+        selection : {
+            mode : 'x'
+        },
+        legend: {
+            position: 'se'
+        },
+        HtmlText : false,
+        title : title
+    };
+
+    // Draw graph with default options, overwriting with passed options
+    function drawGraph (opts) {
+
+        // Clone the options, so the 'options' variable always keeps intact.
+        o = Flotr._.extend(Flotr._.clone(options), opts || {});
+
+        // Return a new graph.
+        return Flotr.draw(
+            container,
+            data,
+            options
+        );
+    }
+
+    graph = drawGraph();
+
+    Flotr.EventAdapter.observe(container, 'flotr:select', function(area){
+        // Draw selected area
+        graph = drawGraph({
+            xaxis : { min : area.x1, max : area.x2, mode : 'time', labelsAngle : 45 },
+            yaxis : { min : area.y1, max : area.y2 }
+        });
+    });
+
+    // When graph is clicked, draw the graph with default area.
+    Flotr.EventAdapter.observe(container, 'flotr:click', function () { graph = drawGraph();});
+}
+
+function collect (key, normalize) {
         var _data = Flotr._.map(DATA,
                     function (d) {
                        return [new Date(d.date).getTime(),
-                               d[key]];
+                               (normalize) ? d[key]/d.words : d[key]];
                     });
         return Flotr._.map(Flotr._.range(0, _data.length, 14),
                            function (i) {
@@ -28,55 +71,37 @@
                                                       function (a,b) { return a+b; })/14.0];
                            });
 
-    };
+};
 
-    d1 = collect('bad_words');
-    d2 = collect('sad_words');
-    d3 = collect('happy_words');
-    d4 = collect('sex_words');
+(function types_time(container) {
+    var
+    d1 = collect('bad_words', false),
+    d2 = collect('sad_words', false),
+    d3 = collect('happy_words', false),
+    d4 = collect('sex_words', false);
 
-    options = {
-        xaxis : {
-            mode : 'time',
-            labelsAngle : 45
-        },
-        selection : {
-            mode : 'x'
-        },
-        legend: {
-            position: 'se'
-        },
-        HtmlText : false,
-        title : 'Word types used (14 day running window)'
-    };
+    draw_time(container, [{data: d1, label: 'Cursewords'},
+                          {data: d2, label: 'Sad words'},
+                          {data: d3, label: 'Happy words'},
+                          {data: d4, label: 'Sex'}],
+              'Word types used (14 day running window)');
 
-    // Draw graph with default options, overwriting with passed options
-    function drawGraph (opts) {
+})(document.getElementById("word-types"));
 
-        // Clone the options, so the 'options' variable always keeps intact.
-        o = Flotr._.extend(Flotr._.clone(options), opts || {});
+(function length(container) {
+    var
+    d1 = collect('words', false);
 
-        // Return a new graph.
-        return Flotr.draw(
-            container,
-            [{data: d1, label: 'Cursewords'},
-             {data: d2, label: 'Sad words'},
-             {data: d3, label: 'Happy words'},
-             {data: d4, label: 'Sex'}],
-            options
-        );
-    }
+    draw_time(container, [{data: d1, label: 'N words'}],
+              'Length (14 day running window)');
 
-    graph = drawGraph();
+})(document.getElementById("length"));
 
-    Flotr.EventAdapter.observe(container, 'flotr:select', function(area){
-        // Draw selected area
-        graph = drawGraph({
-            xaxis : { min : area.x1, max : area.x2, mode : 'time', labelsAngle : 45 },
-            yaxis : { min : area.y1, max : area.y2 }
-        });
-    });
+(function length(container) {
+    var
+    d1 = collect('minutes', false);
 
-    // When graph is clicked, draw the graph with default area.
-    Flotr.EventAdapter.observe(container, 'flotr:click', function () { graph = drawGraph(); });
-})(document.getElementById("length-time"));
+    draw_time(container, [{data: d1, label: 'Minutes'}],
+              'Minutes spent writing (14 day running window)');
+
+})(document.getElementById("time"));
